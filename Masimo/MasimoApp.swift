357fc37fc02c1728bc45b683aa80c9be93d61591
Dipesh Dhakal <dipesh.dhakal4040@gmail.com>
2023-10-25 17:29:03 +1100

    Initial Commit

---
+++ Masimo/MasimoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MasimoApp.swift
+//  Masimo
+//
+//  Created by Dipesh Dhakal on 25/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct MasimoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
